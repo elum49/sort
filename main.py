d59be18eb93cb3bcce7d753e721af92592f6bbef
elume <staspovetko166@gmail.com>
2025-10-23 12:05:25 +0300

    selection sort

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swaps += 1
     return arr, comparisons, swaps
+
+def selection_sort(arr):
+    arr = arr.copy()
+    n = len(arr)
+    comparisons = 0
+    swaps = 0
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            comparisons += 1
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            swaps += 1
+    return arr, comparisons, swaps
