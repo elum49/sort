58de781b6b4a62d40603c69da10138309e4778f6
elume <staspovetko166@gmail.com>
2025-10-23 12:04:26 +0300

    I did the bubble sorting

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import random
 
+def bubble_sort(arr):
+    arr = arr.copy()
+    n = len(arr)
+    comparisons = 0
+    swaps = 0
+    
+    for i in range(n-1):
+        for j in range(n-i-1):
+            comparisons += 1
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swaps += 1
+    return arr, comparisons, swaps
