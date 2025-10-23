67a045b8c29da08e80321e9bfc7a2f3f6e554e63
elume <staspovetko166@gmail.com>
2025-10-23 12:05:59 +0300

    insertion_sort

--- main.py
+++ main.py
@@ -29,3 +29,25 @@ def selection_sort(arr):
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
             swaps += 1
     return arr, comparisons, swaps
+
+def insertion_sort(arr):
+    arr = arr.copy()
+    comparisons = 0
+    swaps = 0
+    
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        
+        while j >= 0:
+            comparisons += 1
+            if arr[j] > key:
+                arr[j+1] = arr[j]
+                swaps += 1
+                j -= 1
+            else:
+                break
+        arr[j+1] = key
+        swaps += 1
+        
+    return arr, comparisons, swaps
